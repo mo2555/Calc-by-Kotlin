542067ca3b98e0e5996cfe812ecf3d7aafd2b4f8
mo2555 <62042431+mo2555@users.noreply.github.com>
2021-08-11 18:24:03 +0200

    Update main.kt

--- main.kt
+++ main.kt
@@ -36,24 +36,27 @@ fun main(args:Array<String>) {
         }
         }
     }
-   calc(num1,num2,op)
+   println(calc(num1,num2,op))
 }
-fun calc(num1:Int,num2:Int,op:String){
+fun calc(num1:Int,num2:Int,op:String):String{
     when(op){
         "+"->{
-            println("The Sum = ${num1+num2}")
+            return "The Sum = ${num1+num2}"
         }
         "-"->{
-            println("The Sub = ${num1-num2}")
+            return "The Subtract = ${num1-num2}"
         }
         "*"->{
-            println("The Mul = ${num1*num2}")
+            return "The Multiply = ${num1*num2}"
         }
         "/"->{
-            println("The Div = ${num1.toDouble()/num2}")
+            return "The Div = ${num1.toDouble()/num2}"
         }
         "%"->{
-            println("The Mod = ${num1%num2}")
+            return "The Mod = ${num1%num2 }"
+        }
+        else ->{
+            return "Operator not true"
         }
     }
 }
