372456f3151fccbe7cdaaf184f5f75d7bba1727d
mo2555 <62042431+mo2555@users.noreply.github.com>
2021-08-11 18:20:00 +0200

    Create main.kt

---
+++ main.kt
@@ -0,0 +1,59 @@
+fun main(args:Array<String>) {
+    var num1: Int = 0
+    var num2: Int = 0
+    var op: String = ""
+    var count = 1
+    while (true) {
+        try {
+            if (count == 1) {
+                println("Enter Number 1 : ")
+                num1 = readLine()!!.toInt()
+                count = 2
+            }
+            if (count == 2) {
+                println("Enter Number 2 : ")
+                num2 = readLine()!!.toInt()
+                count = 3
+            }
+            if (count == 3) {
+                println("Enter operator between ( + , - , * , / or %) : ")
+                op = readLine()!!
+                if (op=="+"||op=="-"||op=="*"||op=="/"||op=="%")
+                    break
+                else
+                    throw Exception()
+            }
+        }catch (e:Exception){
+        if(count==1){
+            println("Number 1 not number")
+        }
+            else if(count==2){
+            println("Number 2 not number")
+        }
+            else
+        {
+            println("Can not find this operator")
+        }
+        }
+    }
+   calc(num1,num2,op)
+}
+fun calc(num1:Int,num2:Int,op:String){
+    when(op){
+        "+"->{
+            println("The Sum = ${num1+num2}")
+        }
+        "-"->{
+            println("The Sub = ${num1-num2}")
+        }
+        "*"->{
+            println("The Mul = ${num1*num2}")
+        }
+        "/"->{
+            println("The Div = ${num1.toDouble()/num2}")
+        }
+        "%"->{
+            println("The Mod = ${num1%num2}")
+        }
+    }
+}
